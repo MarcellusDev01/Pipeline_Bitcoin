d62bce7aec9d2711bde7160e453838a4a5128d2a
MarcellusDev01 <marceloleandroleandrofilho@gmail.com>
2025-11-10 17:21:32 -0300

    Adicionando Coin NO SQL

---
+++ coinSql.py
@@ -0,0 +1,37 @@
+import requests
+from tinydb import TinyDB, Query
+from datetime import datetime
+import time
+
+db = TinyDB('bitcoin_data.json')
+
+
+def extrair():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()
+
+def tranformar(dados_json):
+    
+    valor = dados_json['data'],['amount']
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+    
+    dados_tratados = {"valor": valor, "cripto": criptomoeda, "moeda": moeda, "data_hora": datetime.now().isoformat()}
+    
+    return dados_tratados
+
+
+def load(dados_tratados):
+    db.insert(dados_tratados)
+    print("Dados inseridos no TinyDB com sucesso.")
+    
+    
+    
+    
+if __name__ == "__main__":
+    while True:
+     dados_json = extrair()
+     dados_tratados = tranformar(dados_json)
+     load(dados_tratados)
+     time.sleep(5) # Pause for 5 seconds before the next extraction
